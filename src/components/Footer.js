71b9118bf946a38b0f179f7e7610f033e1c2c136
vitor paiva <paivavitorr@gmail.com>
2024-06-05 16:41:57 -0400

    é

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { EnvelopeIcon, PhoneIcon, MapPinIcon } from '@heroicons/react/solid';
+import { MailIcon, PhoneIcon, LocationMarkerIcon } from '@heroicons/react/solid';
 import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 function Footer() {
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-6 mb-4">
           <div className="flex items-center justify-center">
-            <MapPinIcon className="w-4 h-4 mr-2" />
+            <LocationMarkerIcon className="w-4 h-4 mr-2" />
             <a 
               href="https://www.google.com/maps/place/Norte+Blocos+Artefatos+de+Concreto/@-8.7546794,-63.8500767,20.5z/data=!4m14!1m7!3m6!1s0x92325b4565864ad9:0x9768b8829d518c58!2sR.+Daniela,+3505+-+Igarap%C3%A9,+Porto+Velho+-+RO,+76824-329!3b1!8m2!3d-8.7547117!4d-63.8502774!3m5!1s0x92325b107693a16f:0xf33a89df2673f169!8m2!3d-8.75471!4d-63.8503252!16s%2Fg%2F11t6y9894n?hl=pt-BR&entry=ttu" 
               target="_blank" 
@@ -29,7 +29,7 @@ function Footer() {
             </a>
           </div>
           <div className="flex items-center justify-center">
-            <EnvelopeIcon className="w-4 h-4 mr-2" />
+            <MailIcon className="w-4 h-4 mr-2" />
             <a 
               href="mailto:norteblocos.ro@gmail.com" 
               className="text-gray-400 hover:text-white text-sm sm:text-base md:text-lg"
